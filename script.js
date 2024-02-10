d22234fa8a84cb85aaa985b464525f7032700c85
latheeshshetty <latheeshsshetty12@gmail.com>
2024-02-10 17:09:10 +0530

    style and scroll-up changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//scroll up
+const scrollUp =document.querySelector(".socion-up-arrow");
+
+scrollUp.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    });
+});
+
 const burger=document.querySelector("#burger-menu");
 const ul =document.querySelector("nav ul");
 const nav = document.querySelector("nav");
